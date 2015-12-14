743ac1a3197d0f5635be85bb1c75ddd55c4625c7
Nguyễn Nhật Hoàng <hoangnn93@gmail.com>
2015-12-14 22:56:53 +0700

    Update mail config

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,10 +1,8 @@
 class MessageMailer < ApplicationMailer
-  default from: ENV['MAIL_USER']
 
   def new_message(message)
     @message = message
     @url  = ENV['DOMAIN'] + "conversation?friend_id=" + message.sender.id.to_s
     mail(to: @message.recipient.email, subject: 'You have new message')
   end
-
 end
