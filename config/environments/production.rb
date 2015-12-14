2fbfa5a00e86ab35b8052c019b7550cc76b08570
Nguyễn Nhật Hoàng <hoangnn93@gmail.com>
2015-12-14 23:42:58 +0700

    Update mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               ENV['MAIL_DOMAIN'],
-    user_name:            ENV['MAIL_ID'],
+    user_name:            ENV['MAIL_USER'],
     password:             ENV['MAIL_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true  }
